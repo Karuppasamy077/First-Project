2cd744c260d7ca9c6bc7685a51078ace98be2aac
Karuppasamy <M.Karuppasamy077@gmail.com>
2022-03-22 14:38:01 +0530

    second

--- Employee Project/Project/App/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from App.models import EmployeeTable,Contactus
-
-# Register your models here.
-
-admin.site.register(EmployeeTable)
-admin.site.register(Contactus)
